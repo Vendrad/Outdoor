46962d18fe7bbd837ff70a85ad2cc2f5e141be06
Dane Shenton <dane.shenton@gmail.com>
2018-09-23 08:54:23 +0200

    Added Unity project

---
+++ Assets/DayNightLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightLight : MonoBehaviour
+{
+    public int initX;
+    public int initY;
+    public int initZ;
+    public float x;
+    public int y;
+    public int z;
+
+    private void Start()
+    {
+        if (initX == 0 && initY == 0 && initZ == 0) return;
+        transform.rotation = Quaternion.Euler(new Vector3(initX, initY, initZ));
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(x, y, z));
+    }
+}
---
+++ Assets/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    public int x;
+    public int y;
+    public int z;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(x, y, z));
+	}
+}
